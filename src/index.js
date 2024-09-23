326d148ffe6d33e02b7258e2c06138c49e75ac9c
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-23 20:19:42 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating onSetRating={setMovieRating} />
+      <p>{movieRating}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating />
+    <StarRating maxRating={3} color="blue" messages={["noob", "mid", "pro"]} />
+    <StarRating size={24} color="red" defaultRating={1} />
+    <Test />
   </React.StrictMode>
 );
 
